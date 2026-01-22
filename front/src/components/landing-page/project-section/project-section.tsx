a7ea155dc389390983ca743fb4032b4dd84c03bf
invader237 <diego.trivino.fr@gmail.com>
2026-01-22 23:46:36 +0100

    :lipstick:(landing-page): improve project-card styling

--- front/src/components/landing-page/project-section/project-section.tsx
+++ front/src/components/landing-page/project-section/project-section.tsx
@@ -9,16 +9,26 @@ interface ProjectsSectionProps {
 export default function ProjectsSection({ projects }: ProjectsSectionProps) {
   return (
     <LandingPageSection sectionId="my-projects">
-        <h2 className="text-5xl font-bold text-white">My projects</h2>
-        <p className="text-white/70">
-          Here are some of my personal projects that I&apos;ve worked on to sharpen my skills and explore new technologies.
-        </p>
+      <h2 className="text-5xl font-bold text-white">
+        My projects
+      </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-          {projects.map(p => (
-            <ProjectCard key={p.id} {...p} />
-          ))}
-        </div>
+      <p className="text-white/70 max-w-3xl">
+        Here are some of my personal projects that I&apos;ve worked on
+        to sharpen my skills and explore new technologies.
+      </p>
+
+      <div className="
+          grid w-full
+          grid-cols-1
+          md:grid-cols-2
+          xl:grid-cols-[repeat(3,minmax(420px,1fr))]
+          gap-10
+      ">
+        {projects.map(project => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
+      </div>
     </LandingPageSection>
   );
 }
